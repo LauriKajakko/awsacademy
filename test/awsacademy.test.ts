616c3f52e134dee00c84a495dd16f2e2928c2203
Lauri Kajakko <kajakko.lauri@gmail.com>
2022-06-13 14:07:13 +0300

    Initial commit

---
+++ test/awsacademy.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Awsacademy from '../lib/awsacademy-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/awsacademy-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Awsacademy.AwsacademyStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
